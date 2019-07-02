024c97dac4e100fcd73613bd9f4221752557d977
admin <admin@admins-mbp-2.home>
2019-07-02 15:25:10 -0400

    nav

--- site/snippets/nav.php
+++ site/snippets/nav.php
@@ -1,5 +1,5 @@
 <nav class="main-nav">
 <?php foreach($site->children()->listed() as $item): ?>
-    <a <?php e($item->isActive(), ' class="active"') ?> href="<?php echo $item->url() ?><?php if ($item->isHomePage()): ?><?php else: ?><?php endif ?>" style="background-image: url('<?php if ($cover = $item->cover()->toFile()) { echo $cover->url(); } ?>');"></a>
+    <a <?php e($item->isActive(), ' class="active"') ?> href="<?php echo $item->url() ?><?php if ($item->isHomePage()): ?><?php else: ?>#e<?php endif ?>" style="background-image: url('<?php if ($cover = $item->cover()->toFile()) { echo $cover->url(); } ?>');"></a>
 <?php endforeach ?>
 </nav>
